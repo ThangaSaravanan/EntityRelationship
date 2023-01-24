57a0abcf13cbbf07f4cbdffacdeb37d8c9f98392
ThangaSaravanan <sunilsaravanan123@gmail.com>
2023-01-24 17:10:52 +0530

    Entity Relationship

---
+++ demoStudent/demoStudent/src/main/java/com/StudentGame/demoStudent/DemoStudentApplication.java
@@ -0,0 +1,13 @@
+package com.StudentGame.demoStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStudentApplication.class, args);
+	}
+
+}
---
+++ management/management/src/main/java/com/student/management/repository/StudentRepository.java
@@ -0,0 +1,5 @@
+package com.student.management.repository;
+
+public class StudentRepository {
+
+}
